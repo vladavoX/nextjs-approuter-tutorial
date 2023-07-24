0084fcaf146a98b41e26d11cb25156c67b82245f
Vladimir Aleksic <vlada.pzs@hotmail.com>
2023-07-24 22:30:34 +0200

    Fetch data from local file

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,55 +1,47 @@
-import Button from '@/components/Button/Button'
 import Image from 'next/image'
+import { notFound } from 'next/navigation'
+
+import Button from '@/components/Button/Button'
+
+import { items } from './data'
+
+const getData = cat => {
+	const data = items[cat]
+
+	if (data) return data
+
+	return notFound()
+}
 
 const Category = ({ params }) => {
+	const data = getData(params.category)
 	return (
 		<div>
 			<h1 className='text-lightGreen text-3xl font-semibold'>{params.category}</h1>
 			<div className='[&>*:nth-child(2n+1)]:flex-row-reverse'>
-				<div className='flex gap-12 mt-12 mb-24'>
-					<div className='flex-1 flex flex-col gap-5 justify-center'>
-						<h2 className='text-2xl font-semibold'>Test</h2>
-						<p>
-							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, voluptatum minima. Quo quisquam earum hic,
-							nesciunt in dolore eaque obcaecati sunt quam accusamus, molestias nobis vero sequi reprehenderit eum
-							ipsam!
-						</p>
-						<Button
-							text='See More'
-							url='#'
-						/>
+				{data.map(item => (
+					<div
+						className='flex gap-12 mt-12 mb-24'
+						key={item.id}
+					>
+						<div className='flex-1 flex flex-col gap-5 justify-center'>
+							<h2 className='text-2xl font-semibold'>{item.title}</h2>
+							<p>{item.desc}</p>
+							<Button
+								text='See More'
+								url='#'
+							/>
+						</div>
+						<div className='flex-1 h-[500px] relative'>
+							<Image
+								src={item.image}
+								alt=''
+								fill
+								className='object-cover'
+							/>
+						</div>
 					</div>
-					<div className='flex-1 h-[500px] relative'>
-						<Image
-							src='https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg'
-							alt=''
-							fill
-							className='object-cover'
-						/>
-					</div>
-				</div>
-				<div className='flex gap-12 mt-12 mb-24'>
-					<div className='flex-1 flex flex-col gap-5 justify-center'>
-						<h2 className='text-2xl font-semibold'>Test</h2>
-						<p>
-							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est, voluptatum minima. Quo quisquam earum hic,
-							nesciunt in dolore eaque obcaecati sunt quam accusamus, molestias nobis vero sequi reprehenderit eum
-							ipsam!
-						</p>
-						<Button
-							text='See More'
-							url='#'
-						/>
-					</div>
-					<div className='flex-1 h-[500px] relative'>
-						<Image
-							src='https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg'
-							alt=''
-							fill
-							className='object-cover'
-						/>
-					</div>
-				</div>
+				))}
 			</div>
 		</div>
 	)
